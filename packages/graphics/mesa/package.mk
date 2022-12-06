652b67f5b0a0a93e236c4550ae52b1fe8bd895ce
brooksytech <macebrooks@gmail.com>
2022-12-06 15:09:03 -0800

    Bump mesa to 22.3.0

--- packages/graphics/mesa/package.mk
+++ packages/graphics/mesa/package.mk
@@ -3,10 +3,11 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="mesa"
-PKG_VERSION="d5394296becfc97bc992c82d6f5d013b35b5275a"
+PKG_VERSION="22.3.0"
+PKG_SHA256="644bf936584548c2b88762111ad58b4aa3e4688874200e5a4eb74e53ce301746"
 PKG_LICENSE="OSS"
-PKG_SITE="https://gitlab.freedesktop.org/mesa/mesa"
-PKG_URL="${PKG_SITE}.git"
+PKG_SITE="http://www.mesa3d.org/"
+PKG_URL="https://mesa.freedesktop.org/archive/mesa-${PKG_VERSION}.tar.xz"
 PKG_DEPENDS_TARGET="toolchain expat libdrm Mako:host"
 PKG_LONGDESC="Mesa is a 3-D graphics library with an API."
 PKG_TOOLCHAIN="meson"
