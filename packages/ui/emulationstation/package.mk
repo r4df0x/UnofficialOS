1db78c8bdaa5dff0f81c3b35b726c0b378ea6db9
Tim Flaman <timflaman@outlook.com>
2023-01-16 01:49:59 -0600

    Update ES build instructions for uOS

--- packages/ui/emulationstation/package.mk
+++ packages/ui/emulationstation/package.mk
@@ -42,7 +42,7 @@ PKG_CMAKE_OPTS_TARGET+=" -DENABLE_EMUELEC=1 -DDISABLE_KODI=1 -DENABLE_FILEMANAGE
 # One time setup:
 # ---------------
 # cd ~
-# git clone https://github.com/JustEnoughLinuxOS/emulationstation.git
+# git clone https://github.com/RetroGFX/emulationstation.git
 # cd emulationstation
 # git submodule update --init
 #
@@ -56,7 +56,7 @@ PKG_CMAKE_OPTS_TARGET+=" -DENABLE_EMUELEC=1 -DDISABLE_KODI=1 -DENABLE_FILEMANAGE
 #
 # Run from the device:
 # --------------------
-# Copy ./emulationstation binary found in build.JELOS-<device>.aarch64/emulationstation-*/.install_pkg/usr/bin/
+# Copy ./emulationstation binary found in build.UnofficialOS-<device>.aarch64/emulationstation-*/.install_pkg/usr/bin/
 # Via ssh, run emulationstation with
 # systemctl stop emustation
 # chmod +x ./emulationstation
